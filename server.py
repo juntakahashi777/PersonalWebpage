d2c36a3c488c43cbd8f73376951cf80cb7c43542
Jun Takahashi <jtakahas@princeton.edu>
2015-07-09 00:48:32 -0700

    add flask, render json in template

--- server.py
+++ server.py
@@ -1,18 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 import os
 import json
-from BaseHTTPServer import HTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 jsonFile = open("myWebsites.json", "rw");
 data = json.load(jsonFile);
 
 print data["links_1"][0]["url"];
 
-class MyHandler(SimpleHTTPRequestHandler):
-	def translate_path(self, path):
-		root = os.getcwd()
-		return root
+@app.route("/")
+def homepage():
+	return render_template('index.html', links_1=data["links_1"])
 
-if __name__ == '__main__':
-	httpd = HTTPServer(('127.0.0.1', 8000), MyHandler)
-	httpd.serve_forever()
+if __name__ == "__main__":
+	app.run(debug=True)
