30cfde1172c195ae99a78bdd2f2a4e48e0433ae2
Jun Takahashi <jtakahas@princeton.edu>
2015-07-08 23:26:05 -0700

    simple python http server

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-import sys
+import os
 import json
-import BaseHTTPServer
+from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 jsonFile = open("myWebsites.json", "rw");
@@ -8,20 +8,11 @@ data = json.load(jsonFile);
 
 print data["links_1"][0]["url"];
 
+class MyHandler(SimpleHTTPRequestHandler):
+	def translate_path(self, path):
+		root = os.getcwd()
+		return root
 
-HandlerClass = SimpleHTTPRequestHandler
-ServerClass  = BaseHTTPServer.HTTPServer
-Protocol     = "HTTP/1.0"
-
-if sys.argv[1:]:
-    port = int(sys.argv[1])
-else:
-    port = 8000
-server_address = ('127.0.0.1', port)
-
-HandlerClass.protocol_version = Protocol
-httpd = ServerClass(server_address, HandlerClass)
-
-sa = httpd.socket.getsockname()
-print "Serving HTTP on", sa[0], "port", sa[1], "..."
-httpd.serve_forever()
+if __name__ == '__main__':
+	httpd = HTTPServer(('127.0.0.1', 8000), MyHandler)
+	httpd.serve_forever()
