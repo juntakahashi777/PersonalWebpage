a1244d4e67be1a45e5c8bd90f5c276b2534bbbab
Jun Takahashi <jtakahas@princeton.edu>
2015-07-09 22:05:41 -0700

    implement json element push and pop

--- server.py
+++ server.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template
+from flask import *
 app = Flask(__name__)
 import os
 import json
 
-jsonFile = open("myWebsites.json", "rw");
-data = json.load(jsonFile);
+@app.route("/delete")
+def delete_url():
+	jsonFile = open("myWebsites.json", "r");
+	data = json.load(jsonFile);
 
-print data["links_1"][0]["url"];
+	index = request.args.get('index');
 
-@app.route("/")
+	data["links_1"].pop(int(index)-1);
+	jsonFile = open("myWebsites.json", "w");
+	json.dump(data, jsonFile);
+	jsonFile.close();
+
+	return redirect(url_for('homepage'))
+
+@app.route("/", methods=['GET', 'POST'])
 def homepage():
-	return render_template('index.html', links_1=data["links_1"])
+	jsonFile = open("myWebsites.json", "r");
+	data = json.load(jsonFile);
+
+	if request.method == 'POST':
+		new_url = request.form['url'];
+		entry = {"url": new_url}
+		data["links_1"].append(entry);
+		with open("myWebsites.json", mode='w') as jsonFile:
+			json.dump(data, jsonFile);
+	
+	jsonFile.close();
+	return render_template('index.html', links_1=data["links_1"]);
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
